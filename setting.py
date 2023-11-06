82ec5badb3b396921c20db48c9baf1b94b081561
skomlev <skomlev.ruso@gmail.com>
2023-11-05 23:13:43 -0300

    Update setting.py
    Remove private keys

--- setting.py
+++ setting.py
@@ -3,10 +3,10 @@ import os
 EMAIL = {
     'host': os.environ.get('EMAIL_HOST', 'smtp.gmail.com'),
     'port': os.environ.get('EMAIL_PORT', 587),
-    'sender': os.environ.get('EMAIL_SENDER', 'skomlev.ruso@gmail.com'),
-    'user': os.environ.get('EMAIL_USER', 'skomlev.ruso@gmail.com'),
+    'sender': os.environ.get('EMAIL_SENDER', 'example@gmail.com'),
+    'user': os.environ.get('EMAIL_USER', 'example@gmail.com'),
     # https://support.google.com/accounts/answer/185833?hl=es&sjid=14820134242632080947-SA
-    'password': os.environ.get('EMAIL_PASSWORD', "yzcb zzjw hvna cgis "),
+    'password': os.environ.get('EMAIL_PASSWORD', None),
 }
 
 FILE_PATH = os.environ.get('FILE_PATH', 'data/txns.csv')
